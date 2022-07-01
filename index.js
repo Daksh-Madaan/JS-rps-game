a0c7cd480ef96e155cd87cec0944e634c0ca9a51
Daksh Madaan <dakshmadaan@yahoo.com>
2022-07-01 23:18:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+var player = 'Unset';
+var computer = 'Unset';
+var result = 'Match yet to start';
+
+function choice(){
+    choice = ['rock', 'paper', 'scissors'];
+    player = event.target.name;
+    computer = choice[Math.floor(Math.random() * choice.length)];
+    if (player === computer){
+        result = 'Tie';
+    }else if (player === 'rock' && computer === 'scissors'){
+        result = 'You win';
+        document.getElementById('bg').style.background = '#c3f0c2';
+    }else if (player === 'scissors' && computer === 'paper'){
+        result = 'You win';
+        document.getElementById('bg').style.background = '#c3f0c2';
+    }else if (player === 'paper' && computer === 'rock'){
+        result = 'You win';
+        document.getElementById('bg').style.background = '#c3f0c2';
+    }else {
+        result = 'You lose';
+        document.getElementById('bg').style.background = '#f0c2c2';
+    }
+    document.getElementById("player").innerHTML = player;
+    document.getElementById("computer").innerHTML = computer;
+    document.getElementById("result").innerHTML = result;
+}
